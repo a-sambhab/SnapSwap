42e3cc6a9bbd44cda243b024ebc321bb869ad4f4
a-sambhab <saisambhav21chaini@gmail.com>
2023-12-09 01:42:10 +0530

    Hardhat Boilerplate init

--- powerloom-pooler/index.js
+++ powerloom-pooler/index.js
@@ -11,7 +11,15 @@ const data = {
 const text = JSON.stringify(data);
 const api_key = process.env.LIGHTHOUSE_API_KEY;
 const name = "snapSwapData";
+const epoch_id = 2670
 let cid
+
+const getlatestliquidity = async()=>{
+    console.log(process.env.PROJECT_ID)
+    // const response = await axios.get(`https://uniswapv2-api.powerloom.io/data/${epoch_id}/${process.env.PROJECT_ID}`)
+    const response = await axios.get(`https://uniswapv2-api.powerloom.io/data/${epoch_id}/aggregate_24h_top_tokens_lite:9fb408548a732c85604dacb9c956ffc2538a3b895250741593da630d994b1f27:UNISWAPV2/`)
+    // console.log(response.data)
+}
 
 const setData = async() =>{
     const response = await lighthouse.uploadText(text, api_key, name)
@@ -24,4 +32,5 @@ const getData = async()=>{
     const response = await axios.get(`https://gateway.lighthouse.storage/ipfs/${cid}`)
     console.log(response.data)
 }
-setData();
+// setData();
+getlatestliquidity();
